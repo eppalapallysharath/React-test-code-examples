ef070483f3c2f5afea7cae38085a1ff62d86bcd5
eppalapallysharath <eppalapallysharath@gmail.com>
2025-06-17 23:17:38 +0530

    usecall back hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 // import SumComponent from "./SumComponents";
 import Example from "./Example";
 import Example2 from "./Example2";
+import UseCallback from "./UseCallback";
 
 const App = () => {
   return (
     <div>
       React App
       {/* <SumComponent /> */}
-      <Example2 />
+      {/* <Example2 /> */}
+      <UseCallback />
     </div>
   );
 };
